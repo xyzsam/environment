052fb57808c1e6a2960592c2a11a71af67b2ec0d
Sam Xi <samxi@seas.harvard.edu>
2017-05-17 00:15:52 -0400

    Improve installation script.

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
-#!/bin/tcsh
+#!/usr/bin/env bash
 
-mkdir ~/.envsettings
+echo "Creating envsettings directory..."
+mkdir -p ~/.envsettings
 
+echo "Installing vimrc and vim plugins..."
 cp vim/vimrc ~/.envsettings/.vimrc
 cp -rf vim/vim ~/.envsettings/.vim
 
+rm -rf ~/.vim
+rm -rf ~/.vimrc
+ln -s $HOME/.envsettings/.vimrc ~/.vimrc
+ln -s $HOME/.envsettings/.vim ~/.vim
+
+echo "Installing dircolors..."
 cp terminal/dircolors-solarized/dircolors* ~/.envsettings/
+echo "" >> ~/.cshrc
+echo "eval \`dircolors ~/.envsettings/dircolors.ansi-dark\`" >> ~/.cshrc
+
+echo "Installing GNOME terminal color scheme..."
+# ./terminal/gnome-terminal-colors-solarized/install.sh
